e241c0c04e1957b27780762e5675c8c24557a3e5
michaelshamrell <michaelshamrell2001@gmail.com>
2022-04-26 16:17:07 -0700

    Take notes on using the context method in RSpec.

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -8,21 +8,22 @@ class Card
 end
 
 RSpec.describe Card do 
-  # before do 
-  #   @card = Card.new('Ace', 'Spades')
-  # end
-  def card
-    Card.new('Ace', 'Spades')
-  end
+  let(:card) { Card.new('Ace', 'Spades') }
+
   it 'has a rank' do
     expect(card.rank).to eq('Ace')
     card.rank = 'Queen'
     expect(card.rank).to eq('Queen')
-
   end
 
   it 'has a suit' do
     expect(card.suit).to eq('Spades')
+  end
+
+  it 'has a custom error message' do
+    card.suit = 'Nonsense'
+    comparison = 'Spade'
+    expect(card.suit).to eq(comparison), "Hey, i expected #{comparison} but i got #{card.suit} isntead"
   end
 end
 
